2e82b01c36841a13d375f110d2e351c2ce0ecbe2
Andrew Showers <andrew.altimit@gmail.com>
2023-09-10 16:14:44 -0400

    Process multiple images

--- __init__.py
+++ __init__.py
@@ -3,14 +3,6 @@ from PIL import Image
 import torch
 import numpy as np
 
-# Tensor to PIL
-def tensor2pil(image):
-    return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
-
-# Convert PIL to Tensor
-def pil2tensor(image):
-    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
-
 class ImageRemoveBackgroundRembg:
     def __init__(self):
         pass
@@ -19,7 +11,7 @@ class ImageRemoveBackgroundRembg:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "image": ("IMAGE",),
+                "images": ("IMAGE",),
             },
         }
 
@@ -27,9 +19,27 @@ class ImageRemoveBackgroundRembg:
     FUNCTION = "remove_background"
     CATEGORY = "image"
 
-    def remove_background(self, image):
-        image = pil2tensor(remove(tensor2pil(image)))
-        return (image,)
+    def tensor_to_pil(self, img):
+        if img is not None:
+            i = 255. * img.cpu().numpy().squeeze()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+        return img
+
+    def remove_background(self, images):
+        #create empty tensor with the same shape as images
+        total_images = []
+        for image in images:
+            image = self.tensor_to_pil(image)
+            image = remove(image)
+
+            # convert to tensor
+            out_image = np.array(image.convert("RGB")).astype(np.float32) / 255.0
+            out_image = torch.from_numpy(out_image).unsqueeze(0)
+            total_images.append(out_image)
+
+
+        total_images = torch.cat(total_images, 0)
+        return (total_images,)
 
 
 # A dictionary that contains all nodes you want to export with their names
